3ed68f8abf750374dff3c32158b231b5bf4072f2
Siva K Reddybathula <siva.reddybathula@gmail.com>
2012-12-11 23:54:38 -0800

    updated users table with latest changes

--- db/migrate/20121206070849_create_users.rb
+++ db/migrate/20121206070849_create_users.rb
@@ -1,13 +1,27 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :email
-      t.string :firstname
-      t.string :lastname
-      t.date :dateofbirth
-      t.integer :phone
-
-      t.timestamps
-    end
-  end
+     create_table "users", :force => true do |t|
+         t.integer  "facebook_id",                :null => false
+	 t.string   "email",                      :null => false
+	 t.string   "firstname"
+	 t.string   "middlename"
+	 t.string   "lastname"
+	 t.date     "dateofbirth"
+	 t.integer  "phone"
+	 t.string   "gender",      :limit => 100
+	 t.datetime "created_at",                 :null => false
+	 t.datetime "updated_at",                 :null => false
+     end
+   end
 end
+
+
+
+
+
+
+
+   
+    
+  
+
