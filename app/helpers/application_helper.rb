1345f0752dc0f891d16821f5dbfcc13132f3ea60
Siva K Reddybathula <siva.reddybathula@gmail.com>
2012-12-16 21:57:09 -0800

    Changed name to Collaborrow from Sharathon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title)
-    base_title = 'Sharathon - Welcome to the world of sharing and living socially.'
+    base_title = 'Collaborrow - Power of sharing and collaboration...'
     if page_title.empty?
         base_title
     else
